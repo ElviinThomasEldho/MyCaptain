40ff1ed6230bd1e4bd8111056445ca419f0527c5
ElviinThomasEldho <64719409+ElviinThomasEldho@users.noreply.github.com>
2020-07-02 12:47:53 +0530

    Update FibonacciSeries.py

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -3,13 +3,21 @@ n = int(input("Enter number of elements to be displayed : "))
 
 a=0
 b=1
-print(a)
-print(b)
-n-=2
-while(n!=0):
-    s=a+b
-    a=b
-    b=s
-    print(b)
-    n-=1
+
+if (n==1):
+    print(a)
+else if (n==2):
+    print(a)   
+    print(b)    
+else:
+    print(a)   
+    print(b)   
+    n-=2
+    while(n!=0):
+        s=a+b
+        a=b
+        b=s
+        print(b)
+        n-=1
+    
 
